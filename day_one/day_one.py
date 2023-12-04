6ebdd06870aa3aa77c91196784c8f6ed53a426e5
knedliky <simon.karumbi@gmail.com>
2023-12-04 22:00:04 +1100

    solved day one

---
+++ day_one/day_one.py
@@ -0,0 +1,53 @@
+import numpy as np
+
+with open("./data.txt", "r") as f:
+    strings = f.read().splitlines()
+
+
+def part_one(data: str) -> int:
+    number = ""
+    for i in data:
+        if i.isdigit():
+            number += i
+            break
+
+    for i in data[::-1]:
+        if i.isdigit():
+            number += i
+            break
+
+    return 0 if number == "" else int(number)
+
+
+def part_two(data: str) -> int:
+    number = ""
+    data_copy = data
+    digits = ["one", "two", "three", "four", "five", "six", "seven", "eight", "nine"]
+    for digit in digits:
+        if digit in data:
+            data_copy = (
+                data_copy[: data.find(digit)]
+                + str(digits.index(digit) + 1)
+                + data_copy[data.find(digit) + 1 :]
+            )
+            data_copy = (
+                data_copy[: data.rfind(digit)]
+                + str(digits.index(digit) + 1)
+                + data_copy[data.rfind(digit) + 1 :]
+            )
+
+    for i in data_copy:
+        if i.isdigit():
+            number += i
+            break
+
+    for i in data_copy[::-1]:
+        if i.isdigit():
+            number += i
+            break
+
+    return 0 if number == "" else int(number)
+
+
+print("Answer to part one: ", np.sum([part_one(string) for string in strings]))
+print("Answer to part two: ", np.sum([part_two(string) for string in strings]))
